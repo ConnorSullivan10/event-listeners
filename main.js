e451f9156aefbf7f5136a48b73bd9ce9e1056f4f
Connor Sullivan <cksullvn@gmail.com>
2019-08-27 21:29:58 -0500

    updated buttons so that zoeee and michaels buttons pull up their favorite pies via html and js

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ const pies = [
     {
         name:'Cherry Pie',
         imageUrl: 'https://www.thespruceeats.com/thmb/Mg6LdDudts8nCT4rTO8JtBuv48A=/2592x2592/smart/filters:no_upscale()/slice-of-cherry-pie-with-cherry-on-side-on-plate-169960522-67fcb3cdc8024ea48f2f5a8cd3d2fd8c.jpg',
-        instructor: 'Greg',
+        instructor: 'zoeee',
         iceCream: 'Vanilla',
     },
     {
         name:'Banana Cream Pie',
         imageUrl: 'https://i2.wp.com/www.tastesoflizzyt.com/wp-content/uploads/2018/09/banana-cream-pie-recipe-4.jpg?fit=640%2C640&ssl=1',
-        instructor: 'Zoeee',
+        instructor: 'zoeee',
         iceCream: 'Chocolate',
     },
     {
@@ -40,24 +40,45 @@ const pies = [
 ];
 
 const printToDom = (toPrint, divId) => {
-    document.getElementById(divId).innerHTML += toPrint
+    document.getElementById(divId).innerHTML = toPrint
 }
 
 const pieBuilder = (piesArray) => {
+    let domString = ''
     for (let i = 0; i < piesArray.length; i++) {
         const pie = piesArray[i]
-        const domString= `
+        domString += `
         <div class="card">
             <h2>${pie.name}</h2>
             <img src=${pie.imageUrl} alt='Image of ${pie.name}' />
         </div>
         `
-        printToDom(domString, 'pie-zone')
     }
+    printToDom(domString, 'pie-zone')
 }
 
-document.getElementById('my-button').addEventListener('click', () => {
-    console.log('🖱️')
-})
+//document.getElementById('my-button').addEventListener('click', () => {
+  //  console.log('🖱️')
+//})
 
-pieBuilder(pies)
+const buttonClick = (e) => {
+    console.log(e)
+    //figure out WHO this instructor is for the button we clicked on
+    const instructor = e.target.id
+    //only get those pies from the list of all the pies
+    const selectedPies = []
+    for (let i = 0; i < pies.length; i ++) {
+        const pie = pies[i]
+        if (pie.instructor === instructor) {
+            selectedPies.push(pie)
+        }
+    }
+    //pass small list of pies back into the pie builder
+pieBuilder(selectedPies)
+}
+
+document.getElementById('zoeee').addEventListener('click', buttonClick)
+document.getElementById('michael').addEventListener('click', buttonClick)
+
+
+//pieBuilder(pies)
